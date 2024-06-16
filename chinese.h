86418eb1767357d97b002944f952df9f155ce553
杜群瑶 <2023112626@stu.hit.edu.cn>
2024-06-16 10:19:34 +0800

    Add files via upload

---
+++ chinese.h
@@ -0,0 +1,37 @@
+//
+// Created by PC on 2023/11/21.
+//
+
+#ifndef UNTITLED7_CHINESE_H
+#define UNTITLED7_CHINESE_H
+
+#endif //UNTITLED7_CHINESE_H
+
+#define 整型 int
+#define 短整型 short
+#define 长整型 long
+#define 单精度浮点型 float
+#define 双精度浮点型 double
+#define 字符型 char
+#define 字符串型 string
+#define 无符号 unsigned
+#define 空型 void
+#define 遍历 for
+#define 当 while
+#define 做 do
+#define 返回 return
+#define 单字符输入 getchar
+#define 单字符输出 putchar
+#define 格式化输出 printf
+#define 格式化输入 scanf
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 模 %
+#define 赋值 =
+#define 等于 ==
+#define 主函数 main
+#define 缓冲区 fflush
+#define 标准输入流 stdin
+#define 标准输出流 stdout
